3c76ff3a99adb0ddb2300d65b8210b920b2d651a
Gustav Hases <gustav.hases@gmail.com>
2024-10-15 16:09:14 +0200

    Added WiFi and webserver presenting the sensor values

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include <stdio.h>
-
 #include "SSD1306.h"
 #include "rom/ets_sys.h"
 #include "soc/rtc_cntl_reg.h"
 #include "soc/sens_reg.h"
 #include <DHT.h>
+#include <WebServer.h>
 
 #define SCK  5 // GPIO5  -- SX1278's SCK
 #define MISO 19 // GPIO19 -- SX1278's MISnO
@@ -17,32 +17,134 @@
 #define RST  23 // GPIO14 -- SX1278's RESET
 #define DI0  26 // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND 868E6 // LoRa band used
-
 #define DHTTYPE DHT22 //Sensor used
 #define pinDHT22 13 //DHTPin used on board
+
+const char* hostname = "GarageSensor";
+const char* ssid = "YOUSSID";  // Enter SSID here
+const char* password = "YOURPASSWORD";  //Enter Password here
+
 unsigned int counter = 0;
 
 float temp;
 float hum;
 
-DHT dht_test(pinDHT22, DHTTYPE);
-
+DHT dhtsensor(pinDHT22, DHTTYPE);
+WebServer server(80);
 SSD1306 display(0x3c, 21, 22);
 
-//subroutine
+//subroutines
 void get_temperature_and_humidity() {  
-   temp = dht_test.readTemperature();
-   hum = dht_test.readHumidity();         
+   temp = dhtsensor.readTemperature();
+   hum = dhtsensor.readHumidity();         
    if (isnan(temp) || isnan(hum)) {              // Check if any reads failed and exit early (to try again).
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
    }  
 }
 
+void blink_led() {
+  digitalWrite(25, HIGH); // turn the green LED on (HIGH is the voltage level)
+  delay(1000); // wait for a second  
+  digitalWrite(25, LOW); // turn the green LED off by making the voltage LOW 
+  return;
+}
+
+void initWiFi() {
+  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  WiFi.setHostname(hostname);
+  WiFi.begin(ssid,password);
+}
+
+//The following from https://lastminuteengineers.com/esp32-dht11-dht22-web-server-tutorial/ with minor modifications from me
+String SendHTML(float TempCstat, float Humiditystat) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta charset=\"utf-8\" name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">\n";
+  ptr +="<title>GarageTemp stat</title>\n";
+  ptr +="<style>html { font-family: 'Open Sans', sans-serif; display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
+  ptr +="body{margin-top: 50px;}\n";
+  ptr +="h1 {margin: 50px auto 30px;}\n";
+  ptr +=".side-by-side{display: inline-block;vertical-align: middle;position: relative;}\n";
+  ptr +=".humidity-icon{background-color: #3498db;width: 30px;height: 30px;border-radius: 50%;line-height: 36px;}\n";
+  ptr +=".humidity-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".humidity{font-weight: 300;font-size: 60px;color: #3498db;}\n";
+  ptr +=".temperature-icon{background-color: #f39c12;width: 30px;height: 30px;border-radius: 50%;line-height: 40px;}\n";
+  ptr +=".temperature-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".temperature{font-weight: 300;font-size: 60px;color: #f39c12;}\n";
+  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;right: -20px;top: 15px;}\n";
+  ptr +=".data{padding: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="<script>\n";
+  ptr +="setInterval(loadDoc,200);\n";
+  ptr +="function loadDoc() {\n";
+  ptr +="var xhttp = new XMLHttpRequest();\n";
+  ptr +="xhttp.onreadystatechange = function() {\n";
+  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
+  ptr +="document.getElementById(\"webpage\").innerHTML =this.responseText}\n";
+  ptr +="};\n";
+  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.send();\n";
+  ptr +="}\n";
+  ptr +="</script>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  
+   ptr +="<div id=\"webpage\">\n";
+   
+   ptr +="<h1>GarageTemp stat</h1>\n";
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side temperature-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+   ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
+   ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
+   ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
+   ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
+   ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
+   ptr +="<div class=\"side-by-side temperature\">";
+   ptr +=(int)TempCstat;
+   ptr +="<span class=\"superscript\">°C</span></div>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side humidity-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
+   ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
+   ptr +="<div class=\"side-by-side humidity\">";
+   ptr +=(int)Humiditystat;
+   ptr +="<span class=\"superscript\">%</span></div>\n";
+   ptr +="</div>\n";
+
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+void handle_OnConnect() {
+  server.send(200, "text/html", SendHTML(temp,hum)); 
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+
 void setup() {  
   pinMode(25,OUTPUT); //Output for green LED
   pinMode(13,INPUT); //Input for DHT-sensor
-  dht_test.begin();
+  initWiFi();
+  dhtsensor.begin();
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
   
   Serial.begin(115200);
   while (!Serial)
@@ -62,7 +164,8 @@ void setup() {
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-
+  server.begin();
+  Serial.println("HTTP server started");
   delay(1500);
 }
 
@@ -74,23 +177,24 @@ void loop() {
 
   display.drawString(0, 0, "Sending packet: ");
   display.drawString(90, 0, String(counter));
+  
+  server.handleClient();
 
   get_temperature_and_humidity();
 
   String NodeId = "GarageTemp"; //Friendly name for the device
-  
+  //String IP = WiFi.localIP();
   LoRa.beginPacket(); // send packet
   String msg = "{\"name\":\"DHT_LoRa_Sensor\",\"id\":\"" + NodeId + "\",\"temperature\":" + String(temp) + ", \"humidity\":" + String(hum) +"}"; // Build json string to send
-  digitalWrite(25, HIGH); // turn the green LED on (HIGH is the voltage level)
   LoRa.print(msg); // Send json string
   LoRa.endPacket();
-  delay(1000); // wait for a second  
-  digitalWrite(25, LOW); // turn the green LED off by making the voltage LOW
+  blink_led(); //blink led after transmission
   Serial.println(msg); //print the message to console
   
-  display.drawString(0, 15, String(NodeId));
-  display.drawString(0, 30, "tempc: " + String(temp) + " C");
-  display.drawString(0, 45, "hum: " + String(hum) + " %");
+  display.drawString(0, 12, String(NodeId));
+  display.drawString(0, 24, "temp: " + String(temp) + " C");
+  display.drawString(0, 36, "hum: " + String(hum) + " %");
+  display.drawString(0, 48, "IP: " + String(WiFi.localIP().toString()));
   display.display();
   
   delay(5000); //wait for 5 second
